932d0b22c2d02369793037b145c3336fbb5d888e
scottf <scottfauerbach@gmail.com>
2024-01-22 19:45:14 -0500

    Context on the dispatcher use instead of factory creation time.

--- src/main/java/io/nats/client/impl/VertxDispatcherFactory.java
+++ src/main/java/io/nats/client/impl/VertxDispatcherFactory.java
@@ -1,17 +1,26 @@
 package io.nats.client.impl;
 
 import io.nats.client.MessageHandler;
+import io.vertx.core.Vertx;
 import io.vertx.core.impl.ContextInternal;
 
 public class VertxDispatcherFactory extends DispatcherFactory {
+    private final Vertx vertx;
     private final ContextInternal context;
 
+    public VertxDispatcherFactory(Vertx vertx) {
+        this.vertx = vertx;
+        this.context = null;
+    }
+
+    @Deprecated
     public VertxDispatcherFactory(ContextInternal context) {
+        this.vertx = null;
         this.context = context;
     }
 
     @Override
     NatsDispatcher createDispatcher(NatsConnection natsConnection, MessageHandler messageHandler) {
-        return new VertxDispatcher(natsConnection, messageHandler, context);
+        return new VertxDispatcher(natsConnection, messageHandler, vertx == null ? context : (ContextInternal)vertx.getOrCreateContext());
     }
 }
